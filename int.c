f3490a88dc84048d3d82b1c73046a12a64872a91
Matt Joiner <anacrolix@gmail.com>
2012-04-26 00:14:33 +0800

    Implement int comparison

--- int.c
+++ int.c
@@ -1,5 +1,6 @@
 #include "int.h"
 #include "meme.h"
+#include <assert.h>
 
 Int *int_check(Node *node) {
     return node->type == &int_type ? (Int *)node : NULL;
@@ -18,8 +19,19 @@ static void int_print(Node *n, Printer *p) {
     p->just_atom = true;
 }
 
+static NodeCmp int_compare(Node *_left, Node *_right) {
+    assert(_left->type == &int_type);
+    Int *left = (Int *)_left;
+    if (_right->type != &int_type) return NODE_CMP_NOT_IMPLEMENTED;
+    Int *right = (Int *)_right;
+    if (left->ll < right->ll) return NODE_CMP_LT;
+    else if (left->ll > right->ll) return NODE_CMP_GT;
+    else return NODE_CMP_EQ;
+}
+
 Type const int_type = {
     .name = "int",
     .print = int_print,
+    .compare = int_compare,
 };
 
