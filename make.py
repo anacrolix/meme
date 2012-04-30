5c66ad26fc60fc9fa8955e09adc8537f3ed9702a
Matt Joiner <anacrolix@gmail.com>
2012-04-30 11:57:27 +0800

    Reincarnate make.sh as make.py to handle different compiler flags

---
+++ make.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python 
+
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--compiler', choices=['clang', 'gcc'], default='gcc')
+args = parser.parse_args()
+
+build_args = [
+    args.compiler,
+    '-o meme *.c',
+    '`pkg-config --cflags --libs glib-2.0`',
+    '-std=gnu1x -fplan9-extensions -fdiagnostics-show-option',
+    '-Werror-implicit-function-declaration -Wno-unused-parameter',]
+if args.compiler != 'gcc':
+    build_args.append('-Wno-incompatible-pointer-types')
+build_args.append('-g')
+
+raise SystemExit(subprocess.Popen(' '.join(build_args), shell=True).wait())
+
--- make.sh
+++
@@ -1,6 +0,0 @@
-clang -o meme *.c \
-    `pkg-config --cflags --libs glib-2.0` \
-    -std=gnu1x -fplan9-extensions \
-    -Wall -Wextra -Werror=implicit-function-declaration -Wno-unused-parameter \
-    -Wno-incompatible-pointer-types \
-    -g
